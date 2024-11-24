69ee8232dbfaf7159fa8808fed824dad5a57b409
harlinius <gilmartinm222@gmail.com>
2024-11-24 20:36:07 +0100

    tarea 2 diw

--- DAW - Desarrollo web en entorno cliente/02/js/tablaMultiplicar.js
+++ DAW - Desarrollo web en entorno cliente/02/js/tablaMultiplicar.js
@@ -3,10 +3,10 @@ const mostrarTabla = (event) => {
     const numero = Number(document.getElementById('numero').value);
     if (numero >= 0 && numero <= 10) {
       let tabla = document.getElementById('tabla');
-      let tablaMultiplicar = `<h2>Tabla de multiplicar del número ${numero}</h2>`;
+      let tablaMultiplicar = `<h2>Tabla de dividir del número ${numero}</h2>`;
       tablaMultiplicar += '<ul>';
       for (let i = 0; i <= 10; i++) {
-        tablaMultiplicar += `<li>${numero} * ${i} = ${numero * i}</li>`;
+        tablaMultiplicar += `<li>${numero} / ${i} = ${numero / i}</li>`;
       }
       tablaMultiplicar += '</ul>';
       tabla.innerHTML = tablaMultiplicar;
