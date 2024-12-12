016920a5d74d5d4f23e123e3e20631ff9978c56d
harlinius <gilmartinm222@gmail.com>
2024-12-12 17:44:11 +0100

    fuente

--- DESARROLLO WEB EN ENTORNO SERVIDOR/tarea2.php
+++ DESARROLLO WEB EN ENTORNO SERVIDOR/tarea2.php
@@ -55,12 +55,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>Agenda</title>
 </head>
 
 <body>
-    <div style="width: 40%; height: 40%; margin: auto;">
+    <div style="width: 40%; height: 40%; margin: auto; font-family: 'Montserrat', sans-serif;">
         <h1 style="text-align: center;">Agenda</h1>
+
         <!-- Uso fieldset para agrupar los campos para que quede parecido al ejemplo del enunciado. legend es la etiqueta del texto que sobresale. -->
 
         <!-- advertencia -->
